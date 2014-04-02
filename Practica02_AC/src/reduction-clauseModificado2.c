0e079d518ed23a084b55fcc02337f7b71f8f9a32
elsudano <pepitogrillo148@hotmail.com>
2014-04-02 10:41:12 +0200

    Practica 3 de AC inicialización

---
+++ Practica02_AC/src/reduction-clauseModificado2.c
@@ -0,0 +1,33 @@
+/*
+ * reduction-clause.c
+ *
+ *  Created on: 09/04/2014
+ *      Author: Carlos de la Torre
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _OPENMP
+	#include <omp.h>
+#else
+	#define omp_get_thread_num() 0
+#endif
+int main(int argc, char **argv) {
+	int i, n = 20, a[n], suma = 10;
+	if (argc < 2) {
+		fprintf(std:stderr,"Falta iteraciones\n");
+		exit(-1);
+	}
+	n = atoi(argv[1]);
+	if (n > 20) {
+		n = 20;
+		printf("n=%d\n", n);
+	}
+	for (i = 0; i < n; i++)
+		a[i] = i;
+	#pragma omp parallel for reduction(+:suma)
+	for (i = 0; i < n; i++)
+		suma += a[i];
+	printf("Tras 'parallel' suma=%d\n", suma);
+return 0;
+}
