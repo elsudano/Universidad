fe59951304c69e4ca3738715388ccd371dec7c03
Carlos <cdelatorre@correo.ugr.es>
2018-03-18 12:03:25 +0100

    Clase de ejercicios del tema 1 añadida

--- 04Cuarto/Procesamiento_Digital_de_Seniales_PDS/Tema1/Practica1/src/view_app/views.py
+++ 04Cuarto/Procesamiento_Digital_de_Seniales_PDS/Tema1/Practica1/src/view_app/views.py
@@ -5,17 +5,7 @@
 Aqui es donde se pondrán los menus de la aplicación junto con los botones,
 necesarios para que funcione la primera vista de la aplicación.
 """
-try:
-    from Tkinter import ttk
-    from Tkinter import tkFileDialog
-    from Tkinter import tkMessageBox
-except ImportError:
-    # for python 3.x
-    from tkinter import ttk
-    from tkinter import messagebox
-    from tkinter import filedialog
-from PIL import Image, ImageTk
-from src.view_app.view import View
+from src.view_app.view import *
 
 
 class MainView(View):
@@ -39,16 +29,47 @@ class MainView(View):
         # crea bóton dentro de marco
         self.b_exit = ttk.Button(self._principal_frame, text="Exit")
         # ponemos en la posición 0,1 y que se expanda a SurEste
-        self.b_exit.grid(column=12, row=12, sticky='SE')
+        self.b_exit.grid(column=2, row=1, sticky='SE')
         # agregamos el comando del bóton
         self.b_exit.bind("<Button>", self._controller.exit_application)
         # --------------------------------------------------------------------------------
+        self.b_tema1 = ttk.Button(self._principal_frame, text="Ejercicios teoria tema 1")
+        self.b_tema1.grid(column=0, row=0, sticky='NW')
+        self.b_tema1.bind("<Button>", self._controller.eje_teo_tema1)
         self.b_practica1 = ttk.Button(self._principal_frame, text="Practica 1")
-        self.b_practica1.grid(column=0, row=0, sticky='NW')
+        self.b_practica1.grid(column=1, row=0, sticky='NW')
         self.b_practica1.bind("<Button>", self._controller.practica1)
+        self.b_tema2 = ttk.Button(self._principal_frame, text="Ejercicios teoria tema 2")
+        self.b_tema2.grid(column=0, row=1, sticky='NW')
+        self.b_tema2.bind("<Button>", self._controller.eje_teo_tema2)
         self.b_practica2 = ttk.Button(self._principal_frame, text="Practica 2")
-        self.b_practica2.grid(column=1, row=0, sticky='NW')
+        self.b_practica2.grid(column=1, row=1, sticky='NW')
         self.b_practica2.bind("<Button>", self._controller.practica2)
+
+class EjerciciosTema1View(View):
+    """Ventana que muestra todos los ejercicios que hay en las diapositivas del tema 1 de PDS UGR"""
+
+    def _init_view(self):
+        """Método de creación de vista.
+        """
+        # Creamos el marco
+        self._principal_frame = ttk.Frame(self._window.get(), padding="3 3 12 12")
+        # El marco está en la posición 0,0 de la ventana en el centro
+        self._principal_frame.grid(column=0, row=0, sticky='NESW')
+        # cantidad de columnas que tiene el marco
+        self._principal_frame.columnconfigure(0, weight=1)
+        # cantidad de filas que tiene el marco
+        self._principal_frame.rowconfigure(0, weight=1)
+        # crea bóton dentro de marco
+        self.b_back = ttk.Button(self._principal_frame, text="Back")
+        # ponemos en la posición 0,0 y que se expanda a SurEste
+        self.b_back.grid(column=12, row=12, sticky='SE')
+        # agregamos el comando del bóton
+        self.b_back.bind("<Button>", self._controller.back)
+        # --------------------------------------------------------------------------------
+        self.b_pag8 = ttk.Button(self._principal_frame, text="Ejercicio Pagina 8")
+        self.b_pag8.grid(column=0, row=0, sticky='NW')
+        self.b_pag8.bind("<Button>", self._controller.pag8)
 
 class FirstView(View):
     """Segunda Ventana del programa."""
