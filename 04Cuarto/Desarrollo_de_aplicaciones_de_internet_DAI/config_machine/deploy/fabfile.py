721e8e35fddadb510fd4c2fd789f0f3cb04e96cf
Carlos <cdelatorre@correo.ugr.es>
2018-11-09 12:23:34 +0100

    arreglando la practica despues de perderlo todo

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/deploy/fabfile.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/deploy/fabfile.py
@@ -1,50 +1,47 @@
 import os
 from fabric.api import env, local, run, sudo
+from fabric.operations import put
 
-env.user = 'usuario'
-env.hosts = ['middleware.westeurope.cloudapp.azure.com']
-env.key_filename = '~/.ssh/id_rsa_deploying'
+env.user = 'vagrant'
+env.password = 'vagrant'
+env.hosts = ['localhost:2222']
 
 def _levantar_maquina():
     local('vagrant up --no-provision')
 
-def _destruir_maquina_zeit():
-    local('make clean_deploy')
-
-def _destruir_maquina():
-    local('vagrant destroy --force')
+def _detener_maquina():
+    local('vagrant halt --force')
 
 def _configurar_maquina():
     local('vagrant provision')
-    local('sed "/middleware.westeurope.cloudapp.azure.com/d" ~/.ssh/known_hosts > ~/.ssh/known_hosts')
-    if run('echo $DOMAIN') == '':
-        sudo('echo DOMAIN="' + os.environ['DOMAIN'] + '" >> /etc/environment')
-    if run('echo $URL_BASE') == '':
-        sudo('echo URL_BASE="' + os.environ['URL_BASE'] + '" >> /etc/environment')
-    if run('echo $USER_NEXTCLOUD') == '':
-        sudo('echo USER_NEXTCLOUD="' + os.environ['USER_NEXTCLOUD'] + '" >> /etc/environment')
-    if run('echo $PASS_NEXTCLOUD') == '':
-        sudo('echo PASS_NEXTCLOUD="' + os.environ['PASS_NEXTCLOUD'] + '" >> /etc/environment')
-    if run('echo $PORT') == '':
-        sudo('echo PORT="80"' + ' >> /etc/environment')
+    local('sed "/localhost/d" ~/.ssh/known_hosts > ~/.ssh/known_hosts')
 
 def _ejecutar_aplicacion():
-    local('make run &')
-    local('make deploy &')
-    sudo('~/go/bin/MiddleWare_NextCloud')
+    #run('python3.6 ~/src/main.py')
+    run('flask-3.7 run -h 0.0.0.0 -p 5000')
+    
+def _topost():
+    run('mkdir -p ~/src')
+    put('~/GitHub/Universidad/04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/src', '~/')
 
-def _detener_aplicacion():
-    local('curl http://localhost:8080/exit &')
-    local('curl https://middleware-nextcloud.herokuapp.com/exit &')
-    local('curl https://middlewarenextcloud-ovqbkksdcd.now.sh/exit &')
-    local('curl http://middleware.westeurope.cloudapp.azure.com/exit &')
-
-def deploy():
+def poweron():
     _levantar_maquina()
     _configurar_maquina()
-    _ejecutar_aplicacion()
 
-def undeploy():
-    _detener_aplicacion()
-    _destruir_maquina_zeit()
-    _destruir_maquina() 
+def abracadabra():
+    #_topost()
+    _ejecutar_aplicacion()
+    
+def restart():
+    _configurar_maquina()
+    _ejecutar_aplicacion()
+    
+def poweroff():
+    _detener_maquina()
+
+def remove():
+    poweroff()
+    local('vagrant destroy --force')
+
+def test():
+    run('env')
--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/src/main.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/src/main.py
@@ -1,26 +1,102 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-"""Menú principal de la aplicación.
 
-Con este esqueleto se pretende tener un repositorio listo para empezar a,
-desarrollar casi cualquier tipo de aplicación con una interfaz miníma.
-"""
-from src.view_app.window import Window
-from src.model.models import FirstModel
-from src.controller.controllers import FirstController
-from src.view_app.views import FirstView
+from PIL import Image
+from flask import Flask, send_file
 
+app = Flask(__name__)
+
+# paleta es una lista de 3-tuplas con los valores RGB de cada color de la paleta
+def getColorPaleta(paleta, nColoresPaleta, color):
+	nuevoColor = color % (nColoresPaleta)
+	nPuntosControlPaleta = len(paleta)
+	icolor1 = float(nuevoColor) * float(nPuntosControlPaleta - 1) / float(nColoresPaleta - 1)
+	# print len(paleta)
+	# print int(icolor1)
+	if (int(icolor1) == len(paleta) - 1):  # Devolvemos el último color
+		return paleta[len(paleta) - 1]
+        
+	porcentajeC2 = icolor1 - int(icolor1)
+	porcentajeC1 = 1.0 - porcentajeC2
+	icolor1 = int(icolor1)
+	icolor2 = icolor1 + 1
+        
+	nr = int(paleta[icolor1][0] * porcentajeC1 + paleta[icolor2][0] * porcentajeC2)
+	ng = int(paleta[icolor1][1] * porcentajeC1 + paleta[icolor2][1] * porcentajeC2)
+	nb = int(paleta[icolor1][2] * porcentajeC1 + paleta[icolor2][2] * porcentajeC2)
+        
+	return (nr, ng, nb)
+        
+	
+# Renderiza un fractal de Mandelbrot de (x1, y1) a (x2, y2) de ancho pixeles, iteraciones máximas y lo guarda en el fichero especificado en nombreFicheroPNG (debe tener extesión .png) y permitiendo especificar una paleta de colores: paleta es una lista de 3-tuplas con los valores RGB de cada color de la paleta
+
+def renderizaMandelbrotBonito(x1, y1, x2, y2, ancho, iteraciones, nombreFicheroPNG, paleta, nColoresPaleta):
+	# drawing area
+	xa = x1
+	xb = x2
+	ya = y1 + 0.000001
+	yb = y2
+	maxIt = iteraciones
+	# image size
+	imgx = ancho
+	imgy = int(abs (y2 - y1) * ancho / abs(x2 - x1));
+	image = Image.new("RGB", (imgx, imgy))
+	for y in range(imgy):
+		zy = y * (yb - ya) / (imgy - 1)  + ya
+		for x in range(imgx):
+			zx = x * (xb - xa) / (imgx - 1)  + xa
+			z = zx + zy * 1j
+			c = z
+			for i in range(maxIt):
+				if abs(z) > 2.0: break 
+				z = z * z + c
+			if (i == maxIt - 1):
+				image.putpixel((x, y), (0, 0, 0))  # El nucleo del fractal en negro
+			else:
+				image.putpixel((x, y), getColorPaleta(paleta, nColoresPaleta, i))
+	image.save(nombreFicheroPNG, "PNG")
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/static')
+def static_content():
+    return 'static content'
+
+@app.route('/var/<var>')
+def var_name(var):
+    return 'Value Var is:  %s' % var
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Error 404 página no encontrada'
+
+@app.route('/static-image')
+def static_image():
+    return 'dynamic image'
+
+@app.route('/dynamic-image/<x1>/<y1>/<x2>/<y2>/<w>/<i>/<p>')
+def dynamic_image(x1,y1,x2,y2,w,i,p):
+    print ('This is the values of all vars in URL:')
+    print ('X1: %s' % x1)
+    print ('Y1: %s' % y1)
+    print ('X2: %s' % x2)
+    print ('Y2: %s' % y2)
+    print ('Width: %s' % w)
+    print ('Iterations: %s' % i)
+    print ('Palette Nº: %s' % p)
+    npaleta=int(p)
+    if (npaleta == 1):
+        paleta = [(96,111,140), (80,55,0), (0,100,25)] #cambiar colores
+    elif (npaleta == 2):
+        paleta = [(0,0,0), (255,255,255)]
+    else:
+        paleta = [(255,0,0), (0,255,0), (0,0,255)]
+    #print (paleta)
+    renderizaMandelbrotBonito(float (x1), float (y1), float (x2), float (y2), int (w), int (i), "/home/vagrant/src/imagen.png", paleta, 6)
+    return send_file("/home/vagrant/src/imagen.png", mimetype='image/png')
 
 if __name__ == '__main__':
-    # Creamos la Ventana
-    myWindow = Window("Instalación de Aplicación", 600, 350)
-    # Creamos el modelo
-    myModel = FirstModel()
-    # Creamos el controlador de la vista principal y le añadimos la ventana y el modelo
-    myController = FirstController(myWindow, myModel)
-    # Creamos la vista principal de la aplicación y le añadimos la ventana y el controlador
-    myView = FirstView(myWindow, myController)
-    # inicializamos la vista principal
-    myView.init_view()
-    # Hacemos que se muestre la ventana y hacemos funcionar el bucle principal
-    myWindow.start()
+    app.run(host='0.0.0.0')
+
