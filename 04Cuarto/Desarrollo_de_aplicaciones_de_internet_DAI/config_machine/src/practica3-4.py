1e3cde45f46506d05e92bed6525740dd00b6432f
Carlos <cdelatorre@correo.ugr.es>
2018-11-12 19:18:26 +0100

    Actualizando platillas para incluir el formulario y el menu

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/src/practica3-4.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/config_machine/src/practica3-4.py
@@ -2,59 +2,17 @@
 # -*- coding: UTF-8 -*-
 
 from PIL import Image
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template, request, redirect, url_for, session
 
-app = Flask(__name__, static_folder="static_dir")
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/')
-def hello_world():
-    data = """<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Documentación de la APP</title>
-        <style type="text/css">
-            body, html  { height: 100%; }html, body, div, span, applet, object, iframe,/*h1,2, h3, h4, h5, h6,*/ p, blockquote, pre,a, abbr, acronym, address,del, dfn, em, font, img, ins, kbd, q, s, samp,small, strike, strong, sub, sup, tt, var,b, u, i, center,dl, dt, dd, ol, ul, li,fieldset, form, label, legend,table, caption, tbody, tfoot, thead, tr, th, td {margin: 0;padding: 0;border: 0;outline: 0;font-size: 100%;vertical-align: baseline;background: transparent;}body { line-height: 1; }ol, ul { list-style: none; }blockquote, q { quotes: none; }blockquote:before, blockquote:after, q:before, q:after { content: ''; content: none; }:focus { outline: 0; }del { text-decoration: line-through; }table {border-spacing: 0; }
-            .clear {clear: both;display: block;overflow: hidden;visibility: hidden;width: 0;height: 0;}
-            body{
-                color: #e9e2ee;
-                font-size: 14px;
-                font-family: sans-serif;
-                line-height: 20px;
-                background: #55575c;
-                text-shadow: 0 1px 1px rgba(0,0,0,0.75);
-                -webkit-box-shadow: inset 0 0 300px rgba(0,0,0,0.5);
-                -moz-box-shadow: inset 0 0 300px rgba(0,0,0,0.5);
-                        box-shadow: inset 0 0 300px rgba(0,0,0,0.5);
-            }
-            h1{
-                text-align: center;
-                font-size: 40px;
-            }
-            p{
-                margin-bottom: 0.3em;
-                margin-left: 1em;
-            }
-        </style>
-    </head>
-    <body>
-        <br>
-        <h1>Documentación de la Aplicación</h1>
-        <p>La siguiente aplicación solamente responde a una serie de URLs, de entre las que se destacan las siguientes:</p>
-        <br>
-        <p>/hello --> Muestra la primera plantilla utilizada con Jinja32</p>
-        <p>/var/(nombre) --> En esta dirección nos saldrá un saludo personalizado en el cual sustituirá lo que pongamos en la URL en el lugar donde se encuentra (nombre) por un valor dado, es una forma de probar la entrada de datos a la aplicación</p>
-        <p>/static-data --> En esta dirección lo que vamos a encontrar es una página estática pero esta vez será leida desde un fichero HTML</p>
-        <p>/random-image --> Gnerá una imágen fractal y la muestra en el navegador</p>
-        <p>/dynamic-image/(x1)/(y1)/(x2)/(y2)/(w)/(i)/(p) --> Gnerá una imágen fractal y la muestra en el navegador, pero en está ocasión hay que poner los párametros que queremos para la imágen, quedando de la siguiente manera los datos a pasar: coordenada x1, coordenada y1, coordenada x2, coordenada y2, ancho de la imágen, cantidad de itraciones para el fractal y paleta de colores</p>
-        <br>
-        <p>Por último si no encuentra la página que el usuario esta buscando, la aplicación mostrará una pagina de error personalizada</p>
-    </body>
-    </html>"""
-    return data
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
-@app.route('/hello')
-def hello_template():
-    return "Hola"
+@app.route('/doc')
+def doc():
+    return render_template('doc.html')
 
 @app.route('/var/<myvar>')
 def var_name(myvar):
@@ -102,5 +60,7 @@ def page_not_found(e):
     return data
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', ssl_context='adhoc')
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
 
