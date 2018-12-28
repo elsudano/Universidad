944debd5d840c5d3cf98f2f1eee87b69875ccea7
Carlos <cdelatorre@correo.ugr.es>
2018-12-28 13:37:25 +0100

    Cambios menores

---
+++ 04Cuarto/Sistemas_y_proteccion_de_sistemas_informaticos_SPSI/Practica5/src/view_app/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""Fichero de inicialización del modulo
+
+Este fichero se usa para poder inicializar las diferentes partes del modulo
+se usa para poner los imports necesarios para la aplicación
+"""
+
+from src.view_app.Window import Window
+from src.view_app.View import View
+from src.view_app.Views import MainView
