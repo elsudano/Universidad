06b683228eacc9f423e98aa700df58f03c6be927
Carlos <cdelatorre@correo.ugr.es>
2018-12-28 13:45:25 +0100

    Añadimos funcionalidad para hash en binario

--- 04Cuarto/Sistemas_y_proteccion_de_sistemas_informaticos_SPSI/Practica5/src/model/Models.py
+++ 04Cuarto/Sistemas_y_proteccion_de_sistemas_informaticos_SPSI/Practica5/src/model/Models.py
@@ -12,7 +12,8 @@ class MainModel(Model):
     def hacer_algo(self):
         pass
 
-    def random_hex_string(self, bits=256):
+    @staticmethod
+    def random_hex_string(bits=256):
         # Ejemplos de algoritmos que podemos usar sha3_256(), sha3_384(), sha3_512()
         hex_string = None
         chars = string.ascii_uppercase + string.digits
@@ -26,22 +27,49 @@ class MainModel(Model):
             hex_string = hashlib.sha512(rand_string.encode('utf-8')).hexdigest()
         return str(hex_string)
 
+    @staticmethod
+    def convert_hex_to_bin(hex_string=0xFF):
+        bits_of_string = len(hex_string)*4
+        binary = bin(int(hex_string, 16))[2:].zfill(bits_of_string)
+        return binary
+
+    # for hexadecimal version
     def first_function(self, text, n_bits, string_n_bits):
         bits_of_string = len(string_n_bits)*4
-        id = string_n_bits+text
-        string_x = None
+        myid = string_n_bits+text
         hash_value = None
         count = 0
         while True:
             string_x = self.random_hex_string(bits_of_string)
             count += 1
             if bits_of_string == 256:
-                hash_value = hashlib.sha256((id + string_x).encode('utf-8')).hexdigest()
+                hash_value = hashlib.sha256((myid + string_x).encode('utf-8')).hexdigest()
             if bits_of_string == 384:
-                hash_value = hashlib.sha384((id + string_x).encode('utf-8')).hexdigest()
+                hash_value = hashlib.sha384((myid + string_x).encode('utf-8')).hexdigest()
             if bits_of_string == 512:
-                hash_value = hashlib.sha512((id + string_x).encode('utf-8')).hexdigest()
+                hash_value = hashlib.sha512((myid + string_x).encode('utf-8')).hexdigest()
+            # Justo aquí es donde miramos si la cadena tiene la cantidad de 0 que queremos
             if bits_of_string-int(hash_value, 16).bit_length() == n_bits:
                 break
-        return [id, string_x, hash_value, count]
+        return [myid, string_x, hash_value, count]
 
+    # for binary version
+    def second_function(self, text, n_bits, string_n_bits):
+        bits_of_string = len(string_n_bits)
+        myid = string_n_bits+text
+        hash_value = None
+        count = 0
+        while True:
+            string_x = self.convert_hex_to_bin(self.random_hex_string(bits_of_string))
+            count += 1
+            if bits_of_string == 256:
+                hash_value = hashlib.sha256((myid + string_x).encode('utf-8')).hexdigest()
+            if bits_of_string == 384:
+                hash_value = hashlib.sha384((myid + string_x).encode('utf-8')).hexdigest()
+            if bits_of_string == 512:
+                hash_value = hashlib.sha512((myid + string_x).encode('utf-8')).hexdigest()
+            # Justo aquí es donde miramos si la cadena tiene la cantidad de 0 que queremos
+            if bits_of_string-int(hash_value, 16).bit_length() == n_bits:
+                hash_value = self.convert_hex_to_bin(hash_value)
+                break
+        return [myid, string_x, hash_value, count]
