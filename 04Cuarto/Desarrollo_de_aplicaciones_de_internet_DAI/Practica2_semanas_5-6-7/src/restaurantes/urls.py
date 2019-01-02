e0968abdf9aa155858c4330f26877d2bb945de66
Carlos <cdelatorre@correo.ugr.es>
2019-01-02 12:32:04 +0100

    Versión de la APP con allauth intalado y funcionando

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/urls.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/urls.py
@@ -2,6 +2,25 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.Index, name='index'),
+    path('', views.Login, name='login'),
+    path('', include('allauth.urls')),
+    path('index/', views.Index, name='index'),
+    path('blank/', views.Blank, name='blank'),
+    path('grid/', views.Grid, name='grid'),
+    path('icons/', views.Icons, name='icons'),
+    path('typography/', views.Typography, name='typography'),
+    path('notifications/', views.Notifications, name='notifications'),
+    path('buttons/', views.Buttons, name='buttons'),
+    path('panels-wells/', views.Panels_Wells, name='panels-Wells'),
+    path('forms/', views.Forms, name='forms'),
+    path('tables/', views.Tables, name='tables'),
+    path('morris/', views.Morris, name='morris'),
+    path('delete/', views.Delete, name='delete'),
+    path('edit/', views.Edit, name='edit'),
+    path('update/', views.Update, name='update'),
+    path('new/', views.New, name='new'),
     path('search/', views.Search, name='search'),
+    path('doc/', views.Doc, name='doc'),
+    path('user/', views.User, name='user'),
+    path('settings/', views.Settings, name='settings'),
 ]
--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/views.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/views.py
@@ -1,169 +1,120 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import restaurantes, menu_items
+from django.views.generic import TemplateView
+from .models import menu_items
 from .forms import LoginForm
 
-def _default_response(request):
-    context = {
-        "login_form": LoginForm(request.POST)
-    }
-    return render(request, 'login.html', context)
-
 def Index(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'index.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'index.html', context)
     return result
 
 def Blank(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'blank.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'blank.html', context)
     return result
 
 def Grid(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'grid.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'grid.html', context)
     return result
 
 def Icons(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'icons.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'icons.html', context)
     return result
 
 def Typography(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'typography.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'typography.html', context)
     return result
 
 def Notifications(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'notifications.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'notifications.html', context)
     return result
 
 def Buttons(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'buttons.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'buttons.html', context)
     return result
 
 def Panels_Wells(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'panels-wells.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'panels-wells.html', context)
     return result
 
 def Forms(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'forms.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'forms.html', context)
     return result
 
 def Tables(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'tables.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'tables.html', context)
     return result
 
 def Morris(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'morris.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'morris.html', context)
     return result
 
 def Delete(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "rid": request.session.get('rid'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'delete.html', context)
+    context = {
+        "rid": request.session.get('rid'),
+        "navigation": menu_items,
+    }
+    result = render(request, 'delete.html', context)
     return result
 
 def Edit(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "rid": request.session.get('rid'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'flot.html', context)
+    context = {
+        "rid": request.session.get('rid'),
+        "navigation": menu_items,
+    }
+    result = render(request, 'flot.html', context)
     return result
 
 def Update(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'update.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'update.html', context)
     return result
 
 def New(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'new.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'new.html', context)
     return result
 
 def Search(request):
     iterador = restaurantes.find().limit(10)
     context = {
+        "navigation": menu_items,
         "lista": list(iterador)
     } # Aquí van las variables para la plantilla
     if request.session.get('auth'):
@@ -173,31 +124,26 @@ def Search(request):
     return result
 
 def Doc(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'doc.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'doc.html', context)
     return result
 
 def User(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'user.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'user.html', context)
     return result
 
 def Settings(request):
-    result = _default_response(request)
-    if request.session.get('auth'):
-        context = {
-            "user": request.session.get('user'),
-            "navigation": menu_items,
-        }
-        result = render(request, 'settings.html', context)
+    context = {
+        "navigation": menu_items,
+    }
+    result = render(request, 'settings.html', context)
     return result
+
+def Login(request):
+    response = redirect('/login/')
+    return response
