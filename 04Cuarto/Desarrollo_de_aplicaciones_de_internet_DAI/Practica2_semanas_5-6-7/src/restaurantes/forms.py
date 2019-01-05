e6bcad3db31f0508bf03ab2ba010aafcd97c42d3
Carlos <cdelatorre@correo.ugr.es>
2019-01-05 02:57:46 +0100

    Añadimos los 3 formularios necesarios por ahora

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/forms.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/forms.py
@@ -1,6 +1,33 @@
+# Info from this
+# https://stackoverflow.com/questions/12303478/how-to-customize-user-profile-when-using-django-allauth/12308807#12308807
+# http://notesbyanerd.com/2015/12/19/joint-login-and-signup-django-allauth-view/
 from django import forms
+from allauth.account.forms import LoginForm, SignupForm
 
-class LoginForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-    your_name1 = forms.CharField(label='Your name1', max_length=100)
-    your_name2 = forms.CharField(label='Your name2', max_length=100)
+class MyLoginForm(LoginForm):
+
+    def __init__(self, *args, **kwargs):
+        super(MyLoginForm, self).__init__(*args, **kwargs)
+        self.fields['login'].widget = forms.TextInput(attrs={'class':'form-control'})
+        self.fields['password'].widget = forms.PasswordInput(attrs={'class':'form-control'})
+
+class MySignupForm(SignupForm):
+
+    def __init__(self, *args, **kwargs):
+        super(MySignupForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget = forms.TextInput(attrs={'class':'form-control'})
+        self.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control'})
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control'})
+        
+
+class NewRestaurant(forms.Form):
+    name = forms.CharField(max_length=100, label='Restaurant Name')
+    long = forms.CharField(max_length=100, label='Longitud position')
+    lat = forms.CharField(max_length=100, label='Latitud position')
+
+    def __init__(self, *args, **kwargs):
+        super(NewRestaurant, self).__init__(*args, **kwargs)
+        self.fields['name'].widget = forms.TextInput(attrs={'class':'form-control'})
+        self.fields['long'].widget = forms.TextInput(attrs={'class':'form-control'})
+        self.fields['lat'].widget = forms.TextInput(attrs={'class':'form-control'})
