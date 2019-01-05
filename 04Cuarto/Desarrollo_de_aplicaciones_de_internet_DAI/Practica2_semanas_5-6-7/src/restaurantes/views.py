a41148a3e4fbd339d7a6856a38668500ba67678b
Carlos <cdelatorre@correo.ugr.es>
2019-01-05 03:00:51 +0100

    Preparamos las vistas para que no se puedan ver a menos que haya login

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/views.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica2_semanas_5-6-7/src/restaurantes/views.py
@@ -1,149 +1,199 @@
+# Información útil
+# http://notesbyanerd.com/2015/12/19/joint-login-and-signup-django-allauth-view/
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import TemplateView
 from .models import menu_items
-from .forms import LoginForm
+from .forms import MyLoginForm, MySignupForm, NewRestaurant
 
 def Index(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'index.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'index.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Blank(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'blank.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'blank.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Grid(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'grid.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'grid.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Icons(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'icons.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'icons.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Typography(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'typography.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'typography.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Notifications(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'notifications.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'notifications.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Buttons(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'buttons.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'buttons.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Panels_Wells(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'panels-wells.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'panels-wells.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Forms(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'forms.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'forms.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Tables(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'tables.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'tables.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Morris(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'morris.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'morris.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Delete(request):
-    context = {
-        "rid": request.session.get('rid'),
-        "navigation": menu_items,
-    }
-    result = render(request, 'delete.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'delete.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Edit(request):
-    context = {
-        "rid": request.session.get('rid'),
-        "navigation": menu_items,
-    }
-    result = render(request, 'flot.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'edit.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Update(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'update.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'update.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def New(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'new.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "newform": NewRestaurant(),
+            "navigation": menu_items,
+        }
+        result = render(request, 'new.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Search(request):
     iterador = restaurantes.find().limit(10)
-    context = {
-        "navigation": menu_items,
-        "lista": list(iterador)
-    } # Aquí van las variables para la plantilla
-    if request.session.get('auth'):
-        result = render(request, 'index.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+            "lista": list(iterador)
+        } # Aquí van las variables para la plantilla
+        result = render(request, 'search.html', context)
     else:
-        result = render(request, 'login.html', context)
+        result = redirect('/login/')
     return result
 
 def Doc(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'doc.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'doc.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def User(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'user.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'user.html', context)
+    else:
+        result = redirect('/login/')
     return result
 
 def Settings(request):
-    context = {
-        "navigation": menu_items,
-    }
-    result = render(request, 'settings.html', context)
+    if request.user.is_authenticated:
+        context = {
+            "navigation": menu_items,
+        }
+        result = render(request, 'settings.html', context)
+    else:
+        result = redirect('/login/')
     return result
-
-def Login(request):
-    response = redirect('/login/')
-    return response
