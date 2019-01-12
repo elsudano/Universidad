1f182d9abca275ade0ddc4efb9595b8e62a432f7
Carlos <cdelatorre@correo.ugr.es>
2019-01-12 16:38:56 +0100

    clean code

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica3_semanas_8-9/deploy/fabfile.py
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica3_semanas_8-9/deploy/fabfile.py
@@ -8,10 +8,10 @@ from fabric.contrib.files import exists
 env.pythonbin = 'python3.6'
 env.datenow = datetime.datetime.now().isoformat()
 env.user = 'vagrant'
-env.pass_admin_app = "pbkdf2_sha256$120000$39fUzRVqPpfn$WYKC0q6ZSMXqq6fuFl57wqkRYhEwvXFS31ummVsmScA="
 env.key_filename = "~/.ssh/id_rsa_deploying"
 env.path_django = '/home/vagrant/src'
 env.project = 'DAIPROJECT'
+env.pass_admin_app = os.environ['DJANGO_ADMIN_PASS']
 env.secret_key = os.environ['SECRET_KEY_DJANGO']
 env.mongodb_host = os.environ['MONGODB_HOST']
 env.mongodb_port = os.environ['MONGODB_PORT']
@@ -82,106 +82,103 @@ def _configurar_django():
         run('django-admin startproject %(project)s %(path_django)s' % env)
     if not exists('%(path_django)s/%(name_app)s' % env):
         run('cd %(path_django)s; python3.6 %(path_django)s/manage.py startapp %(name_app)s' % env)
-    run('sed -i "s/\'DIRS\': \\\[\\\]\\\,/\'DIRS\': [os.path.join(BASE_DIR, \'\/templates\/\')],/" \
-    %(path_django)s/%(project)s/settings.py' % env)
-    run('sed -i "s/LANGUAGE_CODE = \'en-us\'/LANGUAGE_CODE = \'es-ES\'/" %(path_django)s/%(project)s/settings.py' % env)
-    run('sed -i "s/TIME_ZONE = \'UTC\'/TIME_ZONE = \'Europe\/Madrid\'/" %(path_django)s/%(project)s/settings.py' % env)
-    run('sed -i "s/SECRET_KEY = \'\'/SECRET_KEY = \'%(secret_key)s\'/" %(path_django)s/%(project)s/settings.py' % env)
-    run('sed -i "s/\'NAME\': \'\',/\'NAME\': \'%(name_dbapp)s\',/" %(path_django)s/%(project)s/settings.py' % env)
+    run("sed -i \"/'DIRS': \[\],/ c'DIRS': [os.path.join(BASE_DIR, '/templates/')],\" \
+    %(path_django)s/%(project)s/settings.py" % env)
+    run("sed -i \"/LANGUAGE_CODE = 'en-us'/ cLANGUAGE_CODE = 'es-ES'\" %(path_django)s/%(project)s/settings.py" % env)
+    run("sed -i \"/TIME_ZONE = 'UTC'/ cTIME_ZONE = 'Europe\/Madrid'\" %(path_django)s/%(project)s/settings.py" % env)
+    run("sed -i \"/SECRET_KEY =/ cSECRET_KEY = '%(secret_key)s'\" %(path_django)s/%(project)s/settings.py" % env)
+    run("sed -i \"/'NAME': '',/ c'NAME': '%(name_dbapp)s',\" %(path_django)s/%(project)s/settings.py" % env)
     debug_on = prompt('¿Quieres el modo debug? [Y/n]?: ', default='Y')
     if debug_on == 'y' or debug_on == 'Y':
-        run('sed -i "s/DEBUG = False/DEBUG = True/" %(path_django)s/%(project)s/settings.py' % env)
-        run('sed -i "s/ALLOWED_HOSTS = \[\'dai.sudano.net\', \'localhost\', \'127.0.0.1\', \'\[::1\]\'\]\
-        /ALLOWED_HOSTS = \[\]/" %(path_django)s/%(project)s/settings.py' % env)
+        run("sed -i \"/DEBUG = False/ cDEBUG = True\" %(path_django)s/%(project)s/settings.py" % env)
     elif debug_on == 'n' or debug_on == 'N':
-        run('sed -i "s/DEBUG = True/DEBUG = False/" %(path_django)s/%(project)s/settings.py' % env)
-        run('sed -i "s/ALLOWED_HOSTS = \[\]\
-        /ALLOWED_HOSTS = \[\'dai.sudano.net\', \'localhost\', \'127.0.0.1\', \'\[::1\]\'\]/" %(path_django)s/%(project)s/settings.py' % env)
-    run('%(pythonbin)s %(path_django)s/manage.py migrate' % env)
-    collect = prompt('¿Quiere realizar la recalección de ficheros estáticos? [y/N]?: ', default='N')
+        run("sed -i \"/DEBUG = True/ cDEBUG = False\" %(path_django)s/%(project)s/settings.py" % env)
+    run("sed -i \"/ALLOWED_HOSTS = \[\]/ cALLOWED_HOSTS = ['dai.sudano.net','localhost','127.0.0.1','[::1]']\" %(path_django)s/%(project)s/settings.py" % env)
+    run("%(pythonbin)s %(path_django)s/manage.py migrate" % env)
+    collect = prompt('¿Quiere realizar la recolección de ficheros estáticos? [y/N]?: ', default='N')
     if collect == 'y' or collect == 'Y':
-        run('%(pythonbin)s %(path_django)s/manage.py collectstatic' % env)
+        run("%(pythonbin)s %(path_django)s/manage.py collectstatic --noinput" % env)
     config_allauth = prompt('¿Quieres configurar AllAuth con MongoDB? [y/N]?: ', default='N')
     if config_allauth == 'y' or config_allauth == 'Y':
         _config_allauth_in_mongodb()
     migrate = prompt('¿Quiere realiazar migraciones para la aplicación %(name_app)s? [y/N]?: ' % env, default='N')
     if migrate == 'y' or migrate == 'Y':
-        run('%(pythonbin)s %(path_django)s/manage.py makemigrations %(name_app)s' % env)
-        run('%(pythonbin)s %(path_django)s/manage.py migrate %(name_app)s' % env)
+        run("%(pythonbin)s %(path_django)s/manage.py makemigrations %(name_app)s" % env)
+        run("%(pythonbin)s %(path_django)s/manage.py migrate %(name_app)s" % env)
     create_user = prompt('¿Quieres crear el super usuario? [y/N]?: ', default='N')
     if create_user == 'y' or create_user == 'Y':
         auto = prompt('¿Modo interactivo o automático? [i/A]?: ', default='A')
         if auto == 'a' or auto == 'A':
-            run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-            "db.auth_user.remove({\'id\':NumberInt(1)})"' % env)
-            run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-            "db.auth_user.insert({\'id\':NumberInt(1),\'password\':\'%(pass_admin_app)s\', \'last_login\':null, \
-            \'is_superuser\':true,\'username\':\'admin\',\'first_name\':\'Administrator\', \
-            \'last_name\':\'of App\',\'email\':\'admin@localhost.local\',\'is_staff\':true, \
-            \'is_active\':true,\'date_joined\':ISODate(\'%(datenow)s\')})"' % env)
+            run("mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+            \"db.auth_user.remove({'id':NumberInt(1)})\"" % env)
+            run("mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+            \"db.auth_user.insert({'id':NumberInt(1),'password':'%(pass_admin_app)s', 'last_login':null, \
+            'is_superuser':true,'username':'admin','first_name':'Administrator', \
+            'last_name':'of App','email':'admin@localhost.local','is_staff':true, \
+            'is_active':true,'date_joined':ISODate('%(datenow)s')})\"" % env)
         elif auto == 'i' or auto == 'I':
-            run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-            "db.auth_user.remove({\'id\':NumberInt(1)})"' % env)
-            run('%(pythonbin)s %(path_django)s/manage.py createsuperuser' % env)
+            run("mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+            \"db.auth_user.remove({'id':NumberInt(1)})\"" % env)
+            run("%(pythonbin)s %(path_django)s/manage.py createsuperuser" % env)
     import_data = prompt('¿Quiere importar los datos de los restaurantes y los vecinos? [y/N]?: ', default='N')
     if import_data == 'y' or import_data == 'Y':
         _import_data_mongodb()
 
 def _config_allauth_in_mongodb():
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.remove({\'id\':NumberInt(1)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.insert({\'id\':NumberInt(1),\'provider\':\'github\',\'name\': \
-    \'Github Dev\',\'client_id\':\'%(github_client_id_dev)s\',\'secret\':\'%(github_client_secret_dev)s\' \
-    ,\'key\':\'\'})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.remove({\'id\':NumberInt(2)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.insert({\'id\':NumberInt(2),\'provider\':\'google\',\'name\': \
-    \'Google Dev\',\'client_id\':\'%(google_client_id_dev)s\',\'secret\':\'%(google_client_secret_dev)s\' \
-    ,\'key\':\'\'})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.remove({\'id\':NumberInt(3)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.insert({\'id\':NumberInt(3),\'provider\':\'github\',\'name\': \
-    \'Github Pro\',\'client_id\':\'%(github_client_id_pro)s\',\'secret\':\'%(github_client_secret_pro)s\' \
-    ,\'key\':\'\'})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.remove({\'id\':NumberInt(4)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp.insert({\'id\':NumberInt(4),\'provider\':\'google\',\'name\': \
-    \'Google Pro\',\'client_id\':\'%(google_client_id_pro)s\',\'secret\':\'%(google_client_secret_pro)s\' \
-    ,\'key\':\'\'})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.django_site.remove({\'id\':NumberInt(1)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.django_site.insert({\'id\':NumberInt(1),\'domain\':\'localhost\',\'name\':\'dai.sudano.net\'})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.remove({\'id\':NumberInt(1)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.insert({\'id\':NumberInt(1),\'socialapp_id\':NumberInt(1),\'site_id\':NumberInt(1)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.remove({\'id\':NumberInt(2)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.insert({\'id\':NumberInt(2),\'socialapp_id\':NumberInt(2),\'site_id\':NumberInt(1)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.remove({\'id\':NumberInt(3)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.insert({\'id\':NumberInt(3),\'socialapp_id\':NumberInt(3),\'site_id\':NumberInt(1)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.remove({\'id\':NumberInt(4)})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.socialaccount_socialapp_sites.insert({\'id\':NumberInt(4),\'socialapp_id\':NumberInt(4),\'site_id\':NumberInt(1)})"' % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.remove({'id':NumberInt(1)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.insert({'id':NumberInt(1),'provider':'github','name': \
+    'Github Dev','client_id':'%(github_client_id_dev)s','secret':'%(github_client_secret_dev)s' \
+    ,'key':''})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.remove({'id':NumberInt(2)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.insert({'id':NumberInt(2),'provider':'google','name': \
+    'Google Dev','client_id':'%(google_client_id_dev)s','secret':'%(google_client_secret_dev)s' \
+    ,'key':''})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.remove({'id':NumberInt(3)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.insert({'id':NumberInt(3),'provider':'github','name': \
+    'Github Pro','client_id':'%(github_client_id_pro)s','secret':'%(github_client_secret_pro)s' \
+    ,'key':''})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.remove({'id':NumberInt(4)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp.insert({'id':NumberInt(4),'provider':'google','name': \
+    'Google Pro','client_id':'%(google_client_id_pro)s','secret':'%(google_client_secret_pro)s' \
+    ,'key':''})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.django_site.remove({'id':NumberInt(1)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.django_site.insert({'id':NumberInt(1),'domain':'localhost','name':'dai.sudano.net'})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.remove({'id':NumberInt(1)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.insert({'id':NumberInt(1),'socialapp_id':NumberInt(1),'site_id':NumberInt(1)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.remove({'id':NumberInt(2)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.insert({'id':NumberInt(2),'socialapp_id':NumberInt(2),'site_id':NumberInt(1)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.remove({'id':NumberInt(3)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.insert({'id':NumberInt(3),'socialapp_id':NumberInt(3),'site_id':NumberInt(1)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.remove({'id':NumberInt(4)})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.socialaccount_socialapp_sites.insert({'id':NumberInt(4),'socialapp_id':NumberInt(4),'site_id':NumberInt(1)})\"" % env)
 
 def _import_data_mongodb():
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval "db.%(name_app)s_restaurants.drop()"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval "db.%(name_app)s_neighborhoods.drop()"' % env)
-    run('mongoimport ~/src/restaurants.json --db %(name_dbapp)s --collection %(name_app)s_restaurants' % env)
-    run('mongoimport ~/src/neighborhoods.json --db %(name_dbapp)s --collection %(name_app)s_neighborhoods' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.%(name_app)s_restaurants.createIndex({name: \'text\'})"' % env)
-    run('mongo mongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
-    "db.%(name_app)s_neighborhoods.createIndex({name: \'text\'})"' % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \"db.%(name_app)s_restaurants.drop()\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \"db.%(name_app)s_neighborhoods.drop()\"" % env)
+    run("mongoimport ~/src/restaurants.json --db %(name_dbapp)s --collection %(name_app)s_restaurants" % env)
+    run("mongoimport ~/src/neighborhoods.json --db %(name_dbapp)s --collection %(name_app)s_neighborhoods" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.%(name_app)s_restaurants.createIndex({name: 'text'})\"" % env)
+    run("mongomongodb://%(mongodb_host)s:%(mongodb_port)s/%(name_dbapp)s --eval \
+    \"db.%(name_app)s_neighborhoods.createIndex({name: 'text'})\"" % env)
 
 def _assing_floating_ip():
     headers = {'Content-Type': 'application/json','Authorization': 'Bearer %(token)s' % env}
@@ -237,6 +234,30 @@ def tests_app(envirotment):
 
 def test(envirotment):
     _assing_floating_ip()
+
+def showvars():
+    print('%(pythonbin)s' % env)
+    print('%(datenow)s' % env)
+    print('%(user)s' % env)
+    print('%(key_filename)s' % env)
+    print('%(path_django)s' % env)
+    print('%(project)s' % env)
+    print('%(pass_admin_app)s' % env)
+    print('%(secret_key)s' % env)
+    print('%(mongodb_host)s' % env)
+    print('%(mongodb_port)s' % env)
+    print('%(token)s' % env)
+    print('%(fip)s' % env)
+    print('%(name_app)s' % env)
+    print('%(name_dbapp)s' % env)
+    print('%(github_client_id_dev)s' % env)
+    print('%(github_client_secret_dev)s' % env)
+    print('%(google_client_id_dev)s' % env)
+    print('%(google_client_secret_dev)s' % env)
+    print('%(github_client_id_pro)s' % env)
+    print('%(github_client_secret_pro)s' % env)
+    print('%(google_client_id_pro)s' % env)
+    print('%(google_client_secret_pro)s' % env)
 
 def install_vbguest(envirotment):
     if envirotment == '':
