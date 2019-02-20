01cb92d797f714c4608945b823bdd18d1d297e94
Carlos <cdelatorre@correo.ugr.es>
2019-02-20 23:18:33 +0100

    Test new function pager from ajax

--- 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica3_semanas_8-9/src/restaurantes/static/js/sb-admin-2.js
+++ 04Cuarto/Desarrollo_de_aplicaciones_de_internet_DAI/Practica3_semanas_8-9/src/restaurantes/static/js/sb-admin-2.js
@@ -45,3 +45,23 @@ $(function() {
         }
     }
 });
+
+// $(function() {
+//     $("#id_username").change(function () {
+//       var username = $(this).val();
+
+//       $.ajax({
+//         url: '/ajax/validate_username/',
+//         data: {
+//           'username': username
+//         },
+//         dataType: 'json',
+//         success: function (data) {
+//           if (data.is_taken) {
+//             alert("A user with this username already exists.");
+//           }
+//         }
+//       });
+
+//     });
+// });
